b7fd8d05d8dfbc98981b1f65b2648099efbcadd7
badx86 <badlolpro@gmail.com>
2023-10-08 02:51:18 +0400

    cookie

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import requests
 
-headers = {"some_header": "123"}
+payload = {"login": "secret_login", "password": "secret_pass"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
 
-response = requests.post("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+cookie_value = response1.cookies.get("auth_cookie")
 
-print(response.text)  # запрос
-print(response.headers)  # ответ
+cookies = {}
+if cookie_value is not None:
+    cookies.update({"auth_cookie": cookie_value})
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
+
+print(response2.text)
+
+# print(response1.status_code)
+# print(dict(response1.cookies))
+
+# headers = {"some_header": "123"}
+#
+# response = requests.post("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+#
+# print(response.text)  # запрос
+# print(response.headers)  # ответ
+
+
