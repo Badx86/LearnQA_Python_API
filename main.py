da575f459d6cb14f04e772656a80f9721c3253cb
badx86 <badlolpro@gmail.com>
2023-10-08 02:29:42 +0400

    headers

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
-first_response = response.history[0]
-second_response = response
+headers = {"some_header": "123"}
 
-print(first_response.url)
-print(second_response.url)
+response = requests.post("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+
+print(response.text)  # запрос
+print(response.headers)  # ответ
