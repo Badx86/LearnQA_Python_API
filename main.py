7c2021919b48f1bc235e6472ecf3a6305f763a0f
badx86 <badlolpro@gmail.com>
2023-10-08 02:22:02 +0400

    status codes

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from json.decoder import JSONDecodeError
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+first_response = response.history[0]
+second_response = response
 
-try:
-    parsed_response_text = response.json()
-    print(parsed_response_text['answer'])
-except JSONDecodeError:
-    print("Response is not a JSON format")
+print(first_response.url)
+print(second_response.url)
