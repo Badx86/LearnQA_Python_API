203719efe06da1e1080290148b109b83a4bc4db8
badx86 <badlolpro@gmail.com>
2023-10-09 03:29:55 +0400

    add docstrings

--- lib/base_case.py
+++ lib/base_case.py
@@ -3,15 +3,27 @@ from requests import Response
 
 
 class BaseCase:
+    """
+    Базовый класс с вспомогательными методами для тестов
+    """
     def get_cookie(self, response: Response, cookie_name):
+        """
+        Получить определенную куку из ответа
+        """
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
     def get_header(self, response: Response, headers_name):
+        """
+        Получить определенный заголовок из ответа
+        """
         assert headers_name in response.headers, f"Cannot find header with name {headers_name} in the last response"
         return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
+        """
+        Получить определенное значение JSON по его имени/ключу из ответа
+        """
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecoderError:
