8fae1e317d390627bd96b3e6de484ad6a8047d07
badx86 <badlolpro@gmail.com>
2023-10-12 01:48:59 +0400

    del useless comments

--- environment.py
+++ environment.py
@@ -25,14 +25,3 @@ class Environment:
 
 ENV_OBJECT = Environment()
 
-"""
-$env:ENV = "prod"
-echo $env:ENV
-python -m pytest tests/
-"""
-"""
-docker --version
-docker pull python 
-docker build -t pytest_runner .
-docker run --rm --mount type=bind,src=C:\\Users\\мвидео\\PycharmProjects\\Test-Automation-REST-API,target=/tests_project/ pytest_runner pytest
-"""
